d39a3ee7731b9b7202a4634bff5fd96938248d1b
Edmund Wang <wangedmund@berkeley.edu>
2021-03-26 04:55:19 -0700

    finished 1/5 in #2

--- transformer_attention.py
+++ transformer_attention.py
@@ -40,11 +40,14 @@ class AttentionQKV(nn.Module):
 
         # As defined is the Attention is all you need paper: https://arxiv.org/pdf/1706.03762.pdf
         key_dim = th.tensor(keys.shape[-1],dtype=th.float32)
-        similarity =  # Compute the similarity according to the QKV formula
-
+        tensorLength = len(keys.shape)
+        if tensorLength == 4:
+            similarity = th.matmul(queries, keys.permute(0, 1, 3, 2)) / th.sqrt(key_dim) # Compute the similarity according to the QKV formula
+        else:
+            similarity = th.matmul(queries, keys.permute(0, 2, 1)) / th.sqrt(key_dim)
         masked_similarity = self.apply_mask(similarity, mask=mask) # We give you the mask to apply so that it is correct, you do not need to modify this.
-        weights =  # Turn the similarity into a normalized output. Remember that the last dim contains the features
-        output =  # Obtain the output
+        weights =  nn.Softmax(tensorLength - 1)(masked_similarity) # Turn the similarity into a normalized output. Remember that the last dim contains the features
+        output = th.matmul(weights, values) # Obtain the output
         ####################################  END OF YOUR CODE  ##################################
 
         return output, weights
@@ -103,11 +106,11 @@ class MultiHeadProjection(nn.Module):
         batch_size, tensorlen = tensor.shape[0], tensor.shape[1]
         feature_size = tensor.shape[2]
 
-        new_feature_size =  # Compute what the feature size per head is.
+        new_feature_size =  ...# Compute what the feature size per head is.
         # Reshape this projection tensor so that it has n_heads, each of new_feature_size
-        tensor = 
+        tensor = ...
         # Transpose the matrix so the outer-dimensions are the batch-size and the number of heads
-        tensor = 
+        tensor = ...
         return tensor
         ##########################################################################################
 
@@ -118,12 +121,12 @@ class MultiHeadProjection(nn.Module):
         # You are given the output from all the heads, and you must combine them back into 1 rank-3 matrix
 
         # Transpose back compared to the split, so that the outer dimensions are batch_size and sequence_length again
-        tensor = 
+        tensor = ...
         batch_size, tensorlen = tensor.shape[0], tensor.shape[1]
         feature_size = tensor.shape[-1]
 
-        new_feature_size =  # What is the new feature size, if we combine all the heads
-        tensor =  # Reshape the Tensor to remove the heads dimension and come back to a Rank-3 tensor
+        new_feature_size =  ...# What is the new feature size, if we combine all the heads
+        tensor = ... # Reshape the Tensor to remove the heads dimension and come back to a Rank-3 tensor
         return tensor
         ##########################################################################################
 
